3185af140d64956fe703afa6a4525d9e2a23c38f
suraj <surajmore2143@gmail.com>
2022-06-17 11:37:28 +0530

    Add  to creat a printmax method generic feature

--- MaximumTest.java
+++ MaximumTest.java
@@ -20,25 +20,17 @@ public class MaximumTest<T extends Comparable<T>> {
         if (z.compareTo(max) > 0) {
             max = z;
         }
+        printMax(x, y, z,max);
         return max;
     }
-
-    public static String testMaximum(String x,String y,String z) {
-        String max = x;
-        if (y.compareTo(max) > 0) {
-            max = y;
-        }
-        if (z.compareTo(max) > 0) {
-            max = z;
-        }
-        return max;
+    public static <T> void printMax(T x ,T y ,T z,T max){
+        System.out.println("Max of %s,%s and %s is %s \n" + x +" "+ y +" "+ z +" is : "+ max);
     }
     public static void main(String[] args) {
         Integer xInt=2, yInt=3,zInt=4;
         Float xF1=6.6f, yF1=8.8f,zF1=7.7f;
         String xStr="pear", yStr="apple",zStr="orange";
 
-        MaximumTest.testMaximum(xStr,yStr ,zStr);
         new MaximumTest(xInt , yInt ,zInt).maximum();
         new MaximumTest(xF1 , yF1 ,zF1).maximum();
         new MaximumTest(xStr , yStr ,zStr).maximum();
